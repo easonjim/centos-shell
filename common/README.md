d0bc7cddcd70b2d6478e2f0c1b0d105737556b0f
Jim <easonjim@163.com>
2018-06-08 11:31:19 +0800

    增加

--- common/README.md
+++ common/README.md
@@ -1,3 +1,5 @@
 # 公共模块
 ## check-root.sh
 检查root用户
+## util.sh
+工具类
--- ssh/edit-port.sh
+++ ssh/edit-port.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
+#
 # 修改端口
+
+# 工具类
+source common/util.sh
+
+# 检查是否为root用户，脚本必须在root权限下运行
+source common/check-root.sh
+
+readonly SSH_FILE_PATH='/etc/ssh/sshd_config'
+
+# 备份
+cp ${SSH_FILE_PATH}{,.bak}
+
+# 开启22端口
+sed -i 's/#Port 22/Port 22/g' ${SSH_FILE_PATH}
+# 增加50022端口
+port=50022
+if [[ -n $1 ]]; then
+  port=$1
+fi
+echo "Port ${port}" >> ${SSH_FILE_PATH}
+# 重启sshd服务
+service sshd reload
