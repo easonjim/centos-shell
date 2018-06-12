2941d869e27bc749838819ae5d65feb9d9532ef3
Jim <easonjim@163.com>
2018-06-13 07:00:22 +0800

    增加

---
+++ firewall/disable-firewall-centos7.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 
+# CentOS 7 Firewall
+
+# 解决相对路径问题
+cd `dirname $0`
+
+# 检查是否为root用户，脚本必须在root权限下运行
+source ../common/check-root.sh
+
+# 关闭防火墙
+systemctl stop firewalld.service 
+systemctl disable firewalld.service 
