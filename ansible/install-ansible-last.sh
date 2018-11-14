2692caf1d074797fd915dd8fbe1f9ca5ac373886
Jim <easonjim@163.com>
2018-11-14 16:33:44 +0800

    add

---
+++ ansible/install-ansible-last.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# node.js 8.11.2
+
+# 解决相对路径问题
+cd `dirname $0`
+
+# 检查是否为root用户，脚本必须在root权限下运行
+source ../common/util.sh
+util::check_root
+
+# 安装依赖
+yum install -y epel-release
+
+# 安装ansible
+yum install -y ansible
+
+# 迁移目录
+mkdir -p /data/service/ansible
+rsync -av /etc/ansible /data/service/
+mv /etc/ansible{,_bak}
+ln -s /data/service/ansible /etc/ansible
---
+++ update-from-ansible.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# 使用ansible更新本脚本，用于批量更新集群环境
+# 前提需要安装ansible
+
+# 检查root
+if [[ "$(whoami)" != "root" ]]; then
+    echo "please run this script as root !" >&2
+    exit 1
+fi
+
+# 定义变量
+GROUPS=$1
+
+[ -z $1 ] && GROUPS=all
+
+ansible ${GROUPS} -m shell -a "cd /root/centos-shell && git pull"
