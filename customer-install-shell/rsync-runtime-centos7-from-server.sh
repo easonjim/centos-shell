0fc313672dcd3803affaacb1b2d20213cad800ba
Jim <easonjim@163.com>
2018-11-14 09:17:14 +0800

    add

--- customer-install-shell/rsync-runtime-centos7-from-server.sh
+++ customer-install-shell/rsync-runtime-centos7-from-server.sh
@@ -13,7 +13,6 @@ util::check_root
 # 定义变量
 TAGER_IP=$1
 TAGER_PORT=$2
-TAGER_ROOT_PASSWD=$3
 if [[ ! -n ${TAGER_IP} ]]; then
   echo "请输入远程IP"
   exit 1
@@ -22,36 +21,23 @@ if [[ ! -n ${TAGER_PORT} ]]; then
   echo "请输入远程端口"
   exit 1
 fi
-if [[ ! -n ${TAGER_ROOT_PASSWD} ]]; then
-  echo "请输入远程root密码"
-  exit 1
-fi
 
 # rsync-file-from-server.sh 远程账号 远程路径 远程IP 远程端口 本地路径
 # 同步环境
-# java
-bash ../rsync/rsync-file-from-server.sh root /data/service/java ${TAGER_IP} ${TAGER_PORT} /data/service/java
-bash ../rsync/rsync-file-from-server.sh root /etc/profile.d/java.sh ${TAGER_IP} ${TAGER_PORT} /etc/profile.d/
-bash ../rsync/rsync-file-from-server.sh root /data/service/maven ${TAGER_IP} ${TAGER_PORT} /data/service/maven
-bash ../rsync/rsync-file-from-server.sh root /etc/profile.d/maven.sh ${TAGER_IP} ${TAGER_PORT} /etc/profile.d/
-bash ../rsync/rsync-file-from-server.sh root /data/service/tomcat ${TAGER_IP} ${TAGER_PORT} /data/service/tomcat
-bash ../rsync/rsync-file-from-server.sh root /etc/init.d/tomcat8 ${TAGER_IP} ${TAGER_PORT} /etc/init.d/
-bash ../rsync/rsync-file-from-server.sh root /etc/profile.d/tomcat.sh ${TAGER_IP} ${TAGER_PORT} /etc/profile.d/
-# node
-bash ../rsync/rsync-file-from-server.sh root /data/service/node ${TAGER_IP} ${TAGER_PORT} /data/service/node
-bash ../rsync/rsync-file-from-server.sh root /etc/profile.d/node.sh ${TAGER_IP} ${TAGER_PORT} /etc/profile.d/
-# nodejs特殊处理pm2
-npm install pm2 -g
-
+bash ../rsync/rsync-file-from-server.sh root /data/service/ ${TAGER_IP} ${TAGER_PORT} /data/service/
+bash ../rsync/rsync-file-from-server.sh root /etc/init.d/ ${TAGER_IP} ${TAGER_PORT} /etc/init.d/
+bash ../rsync/rsync-file-from-server.sh root /etc/profile.d/ ${TAGER_IP} ${TAGER_PORT} /etc/profile.d/
+# 特殊处理
 # ng
-bash ../rsync/rsync-file-from-server.sh root /data/service/nginx ${TAGER_IP} ${TAGER_PORT} /data/service/nginx
-bash ../rsync/rsync-file-from-server.sh root /etc/profile.d/nginx.sh ${TAGER_IP} ${TAGER_PORT} /etc/profile.d/
-bash ../rsync/rsync-file-from-server.sh root /etc/init.d/nginx ${TAGER_IP} ${TAGER_PORT} /etc/init.d/
+source /etc/profile
 # 设置开机启动
 chkconfig nginx on
-# 启动
-service nginx start 
 # 设置用户隶属于www-data用户组
 usermod -aG www-data nginx
-
+# 启动
+service nginx start 
+# tomcat
+# 设置开机启动
+chkconfig --add tomcat8
+# docker
 bash ../docker/install-docker_last-centos7.sh
