bcf993d70eabc41dd1fd908b8abdf763c58b2b90
Jim <easonjim@163.com>
2019-06-12 19:52:28 +0800

    init

--- customer-install-shell/install-runtime-centos7.sh
+++ customer-install-shell/install-runtime-centos7.sh
@@ -13,10 +13,10 @@ util::check_root
 # java
 bash ../java/install-java_1.8.0_172.sh
 bash ../java/install-maven_3.5.3.sh
-bash ../tomcat/install-tomcat_8.5.31.sh
+# bash ../tomcat/install-tomcat_8.5.31.sh
 bash ../java/install-arthas.sh
 # node
-bash ../node/install-node_8.11.2.sh
+# bash ../node/install-node_8.11.2.sh
 # ng
 bash ../nginx/install-nginx_1.14.0.sh
 # docker
